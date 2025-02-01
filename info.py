cf980f12dcba2e8d356ae09ab6465d3893573eb8
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-02-01 13:55:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://bob2025:bob2025@cluster0.vuo6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mogo007:mogo007@cluster0.7o1vfny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ITACHI")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'shiv')
 
